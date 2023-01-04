de5a144fc99bc5e20f850e6b7faad0f482454b6e
gschnabel <40870991+gschnabel@users.noreply.github.com>
2023-01-04 14:00:25 +0100

    include create_DFA_from_rex in __init__.py

--- automaton_stuff/algos/__init__.py
+++ automaton_stuff/algos/__init__.py
@@ -1,5 +1,6 @@
 from .conversion import (
     create_NFA_from_rex,
+    create_DFA_from_rex,
     convert_NFA_to_NFA_without_eps,
     convert_NFA_without_eps_to_DFA,
     convert_DFA_to_minimal_DFA,
