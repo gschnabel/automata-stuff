993ad30e6e82c8a40bd48e1cd5e2a771c98776e8
gschnabel <40870991+gschnabel@users.noreply.github.com>
2023-01-04 16:53:49 +0100

    copartmentalize create_DFA_from_rex

--- automaton_stuff/algos/conversion.py
+++ automaton_stuff/algos/conversion.py
@@ -32,76 +32,101 @@ def _duplicate_automaton_part(
     return clone_state, terminal_states, state_map
 
 
-def _create_NFA_from_rex(automaton, rex, pos=0, cur_state=None):
-    is_most_outer_call = cur_state is None
-    cur_state = automaton.create_state(cur_state)
-    if is_most_outer_call:
-        automaton.set_initial_state(cur_state)
-    while pos < len(rex) and rex[pos] != ')':
-        cursymb = rex[pos]
-        union_pos = locate_union_symb(rex, pos)
-        if union_pos is not None:
-            left_temp_state = automaton.create_state()
-            right_temp_state = automaton.create_state()
-            automaton.add_transition(cur_state, left_temp_state, 'eps')
-            automaton.add_transition(cur_state, right_temp_state, 'eps')
-            left_pos, left_terminal_state = _create_NFA_from_rex(
-                automaton, rex[:union_pos], pos, left_temp_state
-            )
-            right_pos, right_terminal_state = _create_NFA_from_rex(
-                automaton, rex, union_pos+1, right_temp_state
-            )
-            terminal_state = automaton.create_state()
-            automaton.add_transition(left_terminal_state, terminal_state, 'eps')
-            automaton.add_transition(right_terminal_state, terminal_state, 'eps')
-            pos = right_pos
-            if pos < len(rex) and rex[pos] == ')':
-                pos -= 1
-        # treat brackets
-        elif cursymb == '(':
-            temp_state = automaton.create_state()
-            automaton.add_transition(cur_state, temp_state, 'eps')
-            pos, terminal_state = _create_NFA_from_rex(
-                automaton, rex, pos+1, temp_state
-            )
-            if len(rex) == pos or rex[pos] != ')':
-                raise IndexError('missing closing bracket')
-        # treat regular symbol
-        else:
-            if cursymb == '\\':
-                pos += 1
-                cursymb = rex[pos]
-            terminal_state = automaton.create_state()
-            automaton.add_transition(cur_state, terminal_state, cursymb)
-
-        # treat with ?,+,*
-        rex_symb = rex[pos+1] if pos+1 < len(rex) else ''
-        # perform an automaton duplication for `+`
-        if rex_symb == '+':
-            clone_state, new_terminal_states, _ = \
-                _duplicate_automaton_part(automaton, cur_state)
-            automaton.add_transition(terminal_state, clone_state, 'eps')
-            cur_state = clone_state
-            if len(new_terminal_states) != 1:
-                raise IndexError('expect exactly one terminal state here')
-            terminal_state = new_terminal_states.pop()
-        # create appropriate epsilon transitions
-        if rex_symb in ('?', '+', '*'):
-            automaton.add_transition(cur_state, terminal_state, 'eps')
-            if rex_symb in ('+', '*'):
-                automaton.add_transition(terminal_state, cur_state, 'eps')
-            pos += 1
+def _deal_with_rex_modifiers(automaton, rex, pos, cur_state, terminal_state):
+    # treat with ?,+,*
+    rex_symb = rex[pos] if pos < len(rex) else ''
+    # perform an automaton duplication for `+`
+    if rex_symb == '+':
+        clone_state, new_terminal_states, _ = \
+            _duplicate_automaton_part(automaton, cur_state)
+        automaton.add_transition(terminal_state, clone_state, 'eps')
+        cur_state = clone_state
+        if len(new_terminal_states) != 1:
+            raise IndexError('expect exactly one terminal state here')
+        terminal_state = new_terminal_states.pop()
+    # create appropriate epsilon transitions
+    if rex_symb in ('?', '+', '*'):
+        automaton.add_transition(cur_state, terminal_state, 'eps')
+        if rex_symb in ('+', '*'):
+            automaton.add_transition(terminal_state, cur_state, 'eps')
         pos += 1
+    return pos, terminal_state
+
+
+def _deal_with_union_rex(automaton, rex, pos, cur_state):
+    union_pos = locate_union_symb(rex, pos)
+    if union_pos is None:
+        return pos, cur_state
+    left_temp_state = automaton.create_state()
+    right_temp_state = automaton.create_state()
+    automaton.add_transition(cur_state, left_temp_state, 'eps')
+    automaton.add_transition(cur_state, right_temp_state, 'eps')
+    left_pos, left_terminal_state = _create_NFA_from_rex(
+        automaton, rex[:union_pos], pos, left_temp_state
+    )
+    right_pos, right_terminal_state = _create_NFA_from_rex(
+        automaton, rex, union_pos+1, right_temp_state
+    )
+    terminal_state = automaton.create_state()
+    automaton.add_transition(left_terminal_state, terminal_state, 'eps')
+    automaton.add_transition(right_terminal_state, terminal_state, 'eps')
+    pos = right_pos
+    return pos, terminal_state
+
+
+def _deal_with_bracketed_rex(automaton, rex, pos, cur_state):
+    if rex[pos] != '(':
+        return pos, cur_state
+    else:
+        temp_state = automaton.create_state()
+        automaton.add_transition(cur_state, temp_state, 'eps')
+        pos, terminal_state = _create_NFA_from_rex(
+            automaton, rex, pos+1, temp_state
+        )
+        if len(rex) == pos or rex[pos] != ')':
+            raise IndexError('missing closing bracket')
+        pos += 1
+        return pos, terminal_state
+
+
+def _deal_with_symbol(automaton, rex, pos, cur_state):
+    cursymb = rex[pos]
+    if cursymb == '\\':
+        pos += 1
+        cursymb = rex[pos]
+    terminal_state = automaton.create_state()
+    automaton.add_transition(cur_state, terminal_state, cursymb)
+    pos += 1
+    return pos, terminal_state
+
+
+def _create_NFA_from_rex(automaton, rex, pos=0, cur_state=None):
+    specialised_dealers = tuple((
+        _deal_with_union_rex,
+        _deal_with_bracketed_rex,
+        _deal_with_symbol
+    ))
+    while pos < len(rex) and rex[pos] != ')':
+        for current_dealer in specialised_dealers:
+            new_pos, terminal_state = current_dealer(
+                automaton, rex, pos, cur_state
+            )
+            if pos != new_pos:
+                pos, terminal_state = _deal_with_rex_modifiers(
+                    automaton, rex, new_pos, cur_state, terminal_state
+                )
+                break
         cur_state = terminal_state
-        if is_most_outer_call:
-            automaton.set_terminal_states(set((cur_state,)))
     return pos, cur_state
 
 
 def create_NFA_from_rex(rex):
     rex = remove_caret_and_dollar(rex)
     auto = Automaton()
-    _create_NFA_from_rex(auto, rex)
+    initial_state = auto.create_state()
+    auto.set_initial_state(initial_state)
+    pos, terminal_state = _create_NFA_from_rex(auto, rex, 0, initial_state)
+    auto.set_terminal_states(set((terminal_state,)))
     return auto
 
 
