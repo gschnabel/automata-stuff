ef00d6c1aaebf25290a2155199b6c8863ec7a43d
gschnabel <40870991+gschnabel@users.noreply.github.com>
2023-01-05 16:41:29 +0100

    add convenience option to plot_automaton

--- automaton_stuff/utils/visualization.py
+++ automaton_stuff/utils/visualization.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import networkx as nx
 
 
-def plot_automaton(automaton):
+def plot_automaton(automaton, abbreviate_all=True):
     plot_auto = automaton
     edges = list(plot_auto.list_transitions(symbols_in_dict=True))
     G = nx.DiGraph()
@@ -15,9 +15,19 @@ def plot_automaton(automaton):
         labels={node: node for node in G.nodes()},
         connectionstyle="arc3,rad=0.1"
     )
+    alphabet = automaton.list_symbols(include_eps=False)
+    raw_edge_labels = plot_auto.list_transitions(symbols_in_dict=True)
+    edge_labels = dict()
+    for curtrans, symset in raw_edge_labels.items():
+        if abbreviate_all and not alphabet.difference(symset):
+            newsymset = symset.difference(alphabet)
+            newsymset.add('any')
+        else:
+            newsymset = symset.copy()
+        edge_labels[curtrans] = newsymset
     nx.draw_networkx_edge_labels(
         G, pos,
-        edge_labels=plot_auto.list_transitions(symbols_in_dict=True),
+        edge_labels=edge_labels,
         font_color='red',
         label_pos=0.4
     )
